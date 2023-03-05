e52175466637f5b24642ce1230239dee7e0358cc
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-03-05 17:07:20 +0000

    Anastasija Bondare 221RDB395

--- tree_height.py
+++ tree_height.py
@@ -5,7 +5,7 @@ import sys
 import threading
 
 class Node: # Tiek definēta klase Node jeb Mezgls, kas aprakstīs visus mezglus koka struktūrā
-    def __init__(self): # Tiek definēts konstruktora metode, kas tiek izsaukts, kad tiek definēts jauns objekts no klases
+    def  __init__(self): # Tiek definēts konstruktora metode, kas tiek izsaukts, kad tiek definēts jauns objekts no klases
         self.children = [] #Tiek definēts, ka mezgla saraksts ir "children", kas ir sākotnēji ir tukšs, jo nekas netika ievadīts
 
 def compute_height(number, parents): # Tiek definēta jauna fukcija ar diviem parametriem - skaits, vecāki
@@ -15,7 +15,7 @@ def compute_height(number, parents): # Tiek definēta jauna fukcija ar diviem pa
 
     for child_index in range(number): # Ar cikla palīdzību pārbauda katru ievadīto elementu ievadītajā "number" diapozonā
         parent_index = parents[child_index] # Atrod vecāka index jeb vietu, kur tas atrodas sarakstā
-        
+
         if parent_index != -1: # Ar if pārbauda, ja vecāks eksistē,tad 
             nodes[parent_index].children.append(nodes[child_index]) # šim vecākam pievieno klāt bērnu 
         else: # Tomēr, ja neeksistē, tad
@@ -46,7 +46,7 @@ def main():
         with open(ievade, 'r') as file: # nolasa faila saturu un aizver to,
             number = int(file.readline().strip())  # pēc tam nolasa pirmo rindu un pārveido to par Integer jeb skaitli, kas norāda, cik elemntu būs kokā
             parents = list(map(int, file.readline().strip().split())) # un tad otrā rinda arī tiek pārdēvēta par skaitli un atdalīti elementi savā starpā, izmantojot funkciju map()
-            print(compute_height(n, parents)) # Izvada koka augstumu
+            print(compute_height(number, parents)) # Izvada koka augstumu
 
 sys.setrecursionlimit(10**7)  
 threading.stack_size(2**27)   
